19892e975f69fccba2619272188a7f24f95468ff
“Krista <stadler.krista@gmail..com>
2020-03-24 16:28:58 -0600

    Started US 3

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe "As a visitor", type: :feature do
       expect(current_path).to eq("/shelters/#{shelter_1.id}/pets")
     end
 
-    it "I also see a list of reviews for that shelter" do
+    it "I see a list of reviews for that shelter" do
       shelter_1 = Shelter.create(name: "Jordan's Shelter",
                                  address: "123 Fake St.",
                                  city: "Arvada",
@@ -65,5 +65,22 @@ RSpec.describe "As a visitor", type: :feature do
       expect(page).to have_css("img[src*='#{review_2.image}']")
     end
   end
+
+  it "I see a link to add a new review for the shelter"
+  shelter_1 = Shelter.create(name: "Jordan's Shelter",
+                             address: "123 Fake St.",
+                             city: "Arvada",
+                             state: "CO",
+                             zip: 80003)
+  review_1 = shelter_1.reviews.create(title: "Found my new best friend!",
+                                      rating: 5,
+                                      content: "Staff were so helpful and the process was easy.")
+  review_2 = shelter_1.reviews.create(title: "Good experience",
+                                      rating: 4,
+                                      content: "Happy with this animal shelter",
+                                      image: "http://cdn.akc.org/content/article-body-image/norwegianelkhoundpuppy_dog_pictures.jpg")
+  visit "/shelters/#{shelter_1.id}"
+
+  expect(page).to have_link 'Add New Review', href
 
 end

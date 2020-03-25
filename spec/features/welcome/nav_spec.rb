fefa02a9e14697dd50c1c654e8b2de84f11b3b96
“Krista <stadler.krista@gmail..com>
2020-03-25 15:05:32 -0600

    Favorite indicator created and added to nav bar on each page

--- spec/features/welcome/nav_spec.rb
+++ spec/features/welcome/nav_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "As a visitor,", type: :feature do 
-  describe "when I access different pages" do 
-    it "then I see a welcome page." do
+RSpec.describe "As a visitor,", type: :feature do
+  describe "when I access different pages" do
+    it "then I see a pets index, a shelter index, and a favorites indicator." do
       visit "/"
 
       expect(page).to have_link("Pets Index")
       expect(page).to have_link("Shelters Index")
+      expect(page).to have_link("Favorites")
 
       visit "/pets"
 
       expect(page).to have_link("Pets Index")
-      expect(page).to have_link("Shelters Index") 
+      expect(page).to have_link("Shelters Index")
+      expect(page).to have_link("Favorites")
 
       visit "/shelters"
 
       expect(page).to have_link("Pets Index")
       expect(page).to have_link("Shelters Index")
+      expect(page).to have_link("Favorites")
+
     end
-  end 
-end 
+  end
+end

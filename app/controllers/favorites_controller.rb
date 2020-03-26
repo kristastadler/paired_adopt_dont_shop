6e6b68c0ce9573ab96a7129c102d759039bb321d
“Krista <stadler.krista@gmail..com>
2020-03-26 14:40:47 -0600

    Cleaned up files

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,17 +10,4 @@ class FavoritesController < ApplicationController
     flash[:notice] = "#{pet.name} has been added to your favorites list."
     redirect_to "/pets/#{pet.id}"
   end
-
 end
-
-
-def update
-    song = Song.find(params[:song_id])
-    @cart = Cart.new(session[:cart])
-    @cart.add_song(song.id)
-    session[:cart] = @cart.contents
-    quantity = @cart.count_of(song.id)
-    flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{song.title} in your cart."
-    redirect_to songs_path
-  end
-#if / else statement to only increment if value of hash is 0.
